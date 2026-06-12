86559e5fed8b243375c92e2ca95ea2152deb90d7
Intellisys <intellisys@Intellisyss-MacBook-Pro-17.local>
2026-06-12 17:49:19 -0400

    feat: initial work, creating first db models and migrating them to the database

--- expense_tracker_api_project/expense_tracker_api/models.py
+++ expense_tracker_api_project/expense_tracker_api/models.py
@@ -1,3 +1,30 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    category_name = models.CharField(max_length=255, null=True)
+    date_modified = models.DateField(null=True)
+    date_created = models.DateField(null=True)
+    #created_by
+
+class Expense(models.Model):
+    expense_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    amount = models.FloatField(null=True)
+    description = models.CharField(max_length=255, null=True)
+    date_modified = models.DateField(null=True)
+    date_created = models.DateField(null=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="expenses"
+    )
+    #created_by
